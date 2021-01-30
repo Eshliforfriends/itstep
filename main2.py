af6c9fedb181abac180b14fb7f4bbf8b2a4e036b
Eshliforfriends <78106599+Eshliforfriends@users.noreply.github.com>
2021-01-30 16:08:49 +0200

    Delete main2.py

--- main2.py
+++
@@ -1,8 +0,0 @@
-print("My terminal skills are masterpiece")
-
-
-
-
-
-[A[A[A[A[A[D[D[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
-0

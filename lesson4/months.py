4a601dce6d472602590f416e5ec1c5737b96a298
Eshliforfriends <78106599+Eshliforfriends@users.noreply.github.com>
2021-02-01 23:20:46 +0200

    Update months.py

--- lesson4/months.py
+++ lesson4/months.py
@@ -1,12 +1,12 @@
 try:
     x = int(input('Enter month number: '))
     months = ['January', 'February', 'March', 
-    'April', 'May', 'June', 
-    'July', 'August', 'September', 
-    'October', 'November', 'December']
+            'April', 'May', 'June', 
+            'July', 'August', 'September', 
+            'October', 'November', 'December']
     if 1 <= x <= 12:
         print(months[x-1])
     else:
-        print('You made mistake,repeat with integer numbers from 1 to 12')
+        print('Repeat with integer number from 1 to 12')
 except:
-    print('You made mistake,repeat with integer numbers from 1 to 12')
+    print('Repeat with integer number')

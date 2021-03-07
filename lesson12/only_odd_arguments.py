9d050144c8794a5a12f19878d2a3589d3bf5a8d0
alinamaliarchuk <a.maliarchuk.l@gmail.com>
2021-03-07 10:10:59 +0200

    lesson12

--- lesson12/only_odd_arguments.py
+++ lesson12/only_odd_arguments.py
@@ -2,28 +2,27 @@ def only_odd_arguments(func):
     def wrapper_odd(*args):
         n = 0
         for i in args:
-            if i % 2 != 0:
-                if n <= len(args):
-                    n +=1
-                    print(n)
-                    continue
+            for j in args:
+                if j % 2 != 0:
+                    if n <= len(args):
+                        n += 1
+                        continue
+                    else:
+                        return print(func(*args))
                 else:
-                    print(func(*args))
-            else:
-                print('Please enter odd numbers')
-                break
+                    break
     return wrapper_odd
 
 @only_odd_arguments
 def add(a, b):
    return a + b
 
-# add(4, 4)
+add(4, 4)
 add(5, 5)
-#
-# @only_odd_arguments
-# def multiple(a, b, c, d, e):
-#    return a*b*c*d*e
 
-# multiple(4*2*7*1*5)
-# multiple(7*3*11*9*3)
+@only_odd_arguments
+def multiple(a, b, c, d, e):
+   return a*b*c*d*e
+
+multiple(4,2,7,1,5)
+multiple(7,3,11,9,3)

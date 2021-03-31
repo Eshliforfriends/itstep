51758cf25dce7417b79a997d3bbba692beddf797
alinamaliarchuk <a.maliarchuk.l@gmail.com>
2021-03-31 21:41:15 +0300

    lesson22

--- practice/lesson22.py
+++ practice/lesson22.py
@@ -1,18 +1,17 @@
 class CloneTrooper:
-    id = None
+    name = None
     type_id = 'CT'
-    def __init__(self, id):
-        self._id = id
+    def __init__(self, identificator):
+        self.name = f'CT-{identificator}'
+        self._id = identificator
     def say_name(self):
-        print(f'I am a trooper {CloneTrooper.type_id}-{self._id}')
-    def name(self):
-        print(f'{CloneTrooper.type_id}-{self._id}')
+        print(f'I am a trooper {CloneTrooper.type_id}-{self.name }')
     def fight(self):
-        print(f'Trooper {CloneTrooper.type_id}-{self._id} start fighting')
+        print(f'Trooper {CloneTrooper.type_id}-{self.name } start fighting')
 
 
 
 standard_trooper = CloneTrooper(1437)
 standard_trooper.say_name()
-standard_trooper.name()
+print(standard_trooper.name)
 standard_trooper.fight()

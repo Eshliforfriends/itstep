9291f199ef6a14835b3d514e9eff577e0f8a5ed5
alinamaliarchuk <a.maliarchuk.l@gmail.com>
2021-03-31 22:09:11 +0300

    lesson22

--- practice/lesson22.py
+++ practice/lesson22.py
@@ -2,21 +2,24 @@ class CloneTrooper:
     name = None
     type_id = 'CT'
     def __init__(self, identificator):
-        self.name = f'{CloneTrooper.type_id}-{identificator}'
+        self.name = f'{self.type_id}-{identificator}'
         self._id = identificator
+
     def say_name(self):
         print(f'I am a trooper {self.name }')
+
     def fight(self):
         print(f'Trooper {self.name } start fighting')
 
 class StormTrooper(CloneTrooper):
-    CloneTrooper.type_id = 'ST'
+    type_id = 'ST'
+
     @staticmethod
     def type():
         return 'Trooper type -> StormTrooper'
 
 print(StormTrooper.type())
-standard_trooper = CloneTrooper(1437)
+standard_trooper = StormTrooper(1437)
 standard_trooper.say_name()
 print(standard_trooper.name)
 standard_trooper.fight()

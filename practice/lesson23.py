dc515883ec90fac1720a11a750179e867ef91cf8
alinamaliarchuk <a.maliarchuk.l@gmail.com>
2021-04-05 21:48:08 +0300

    lesson23

--- practice/lesson23.py
+++ practice/lesson23.py
@@ -4,15 +4,14 @@ class OrcArmy:
         self.damage_per_orc = damage_per_orc
         self.warrior_health = warrior_health
 
+    def __str__(self):
+        return f'{self.warrior_amount}, {self.damage_per_orc}, {self.warrior_health}'
+
+
     def __add__(self, other):
-        total_warrior = self.warrior_amount + other.warrior_amount
-        return total_warrior
-
-    def average_orc(self,other):
-        return (self.warrior_amount*self.damage_per_orc + other.warrior_amount*other.damage_per_orc)/(self.warrior_amount+other.warrior_amount)
-
-    def average_health(self, other):
-        return (self.warrior_amount * self.warrior_health + other.warrior_amount * other.warrior_health) / (self.warrior_amount+other.warrior_amount)
+        return OrcArmy(self.warrior_amount + other.warrior_amount,
+                       (self.warrior_amount * self.damage_per_orc + other.warrior_amount * other.damage_per_orc) / (self.warrior_amount + other.warrior_amount),
+                       (self.warrior_amount * self.warrior_health + other.warrior_amount * other.warrior_health)/(self.warrior_amount + other.warrior_amount))
 
     def __sub__(self, other):
         if self.warrior_amount > other.warrior_amount:
@@ -26,9 +25,8 @@ class OrcArmy:
 if __name__ == '__main__':
     war1= OrcArmy(4,3,2)
     war2 = OrcArmy(10, 6, 3)
-    print(f'Total warriors {war1 + war2}')
-    print(f'Average damage per orc is {OrcArmy.average_orc(war1, war2)}')
-    print(f'Average warrior health is {OrcArmy.average_health(war1, war2)}')
-    print(f'Difference in warriors amount is {war1-war2}')
+    new_army = war1 + war2
+    print(new_army)
+
 
 
